43504742a828f2b2180ffe0e5dcfdf5c64284186
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-10-01 13:06:18 -0400

    no way

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Flex, Heading, Stack } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
